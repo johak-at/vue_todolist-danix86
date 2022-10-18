16779e2912b0bd73168c98051bb03849c5edd9b2
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2022-10-18 10:55:45 +0200

    added props & emits

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,9 +1,11 @@
 <script setup>
 const props = defineProps(["name", "id"]);
-const emit = defineEmits(["remove"]);
+const emits = defineEmits(["delete"]);
 </script>
 
 <template>
-    {{name}}
-    <button @click="emit('remove', 'id' )">x</button>
+  <li>
+    {{ name }}
+    <button @click="emits('delete', id)">x</button>
+  </li>
 </template>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,46 +1,54 @@
 <script setup>
 import { ref } from "vue";
 import { computed } from "vue";
-import ToDo from './ToDo.vue';
+import ToDo from "./ToDo.vue";
 const list = ref([]);
 const newItem = ref("");
 const filter = ref("");
 const filteredList = computed(() => {
-    let result = [];
-    for (let i = 0; i < list.value.length; i++) {
-        if (list.value[i].name.toLowerCase().includes(filter.value.toLowerCase())) {
-            result.push(list.value[i]);
-        }
+  let result = [];
+  for (let i = 0; i < list.value.length; i++) {
+    if (list.value[i].name.toLowerCase().includes(filter.value.toLowerCase())) {
+      result.push(list.value[i]);
     }
-    return result;
+  }
+  return result;
 });
 function add() {
-    list.value.push({
-        id: Date.now().toString(16),
-        name: newItem.value,
-    });
-    newItem.value = "";
+  list.value.push({
+    id: Date.now().toString(16),
+    name: newItem.value,
+  });
+  newItem.value = "";
 }
 function remove(id) {
-    for (let i = 0; i < list.value.length; i++) {
-        if (list.value[i].id === id) {
-            list.value.splice(i, 1);
-        }
+  for (let i = 0; i < list.value.length; i++) {
+    if (list.value[i].id === id) {
+      list.value.splice(i, 1);
     }
+  }
+}
+
+function dummy(param) {
+  console.log(param);
 }
 </script>
 
 <template>
-    <div>
-        <h1>ToDoList</h1>
-        <input @keyup.enter="add" v-model="newItem" />
-        <br /><br />
-        <input type="text" placeholder="filter list" v-model="filter" />
-        <ul>
-            <ToDo v-for="item in filteredList" :name="item.name" :id="item.id" :key="item.id" @remove="remove(id)">
-            </ToDo>
-        </ul>
-
-
-    </div>
+  <div>
+    <h1>ToDoList</h1>
+    <input @keyup.enter="add" v-model="newItem" />
+    <br /><br />
+    <input type="text" placeholder="filter list" v-model="filter" />
+    <ul>
+      <ToDo
+        v-for="item in filteredList"
+        :name="item.name"
+        :id="item.id"
+        :key="item.id"
+        @delete="remove"
+      >
+      </ToDo>
+    </ul>
+  </div>
 </template>
